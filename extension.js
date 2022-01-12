fe284ae4b3c751aef327c01c889298aebe61dd57
Jack Footne <jackrfootner@gmail.com>
2022-01-12 20:08:35 +1030

    Implement Hover Provider

--- extension.js
+++ extension.js
@@ -11,6 +11,15 @@ const osvar = process.platform;
 const evasRepo = "https://github.com/Heroj04/ev-as";
 const internalConfigPath = path.join(__dirname, "internalConfig.json")
 const internalConfig = fs.existsSync(internalConfigPath) ? JSON.parse(fs.readFileSync(internalConfigPath)) : {}
+const ev_scripts = JSON.parse(fs.readFileSync(path.join(__dirname, "ev_scripts.json")))
+const argTypes = {
+	0: "CmdType",
+    1: "Value",
+    2: "Work",
+    3: "Flag",
+    4: "SysFlag",
+    5: "String"
+}
 
 // Get ev-as from Git
 spawn("git", ["-C", evasPath, "pull", evasRepo + ".git"]);
@@ -222,6 +231,72 @@ function activate(context) {
 				})
 			}
 		})
+	}))
+
+	// Register Hover Provider Method
+	context.subscriptions.push(vscode.languages.registerHoverProvider("evscript", {
+		provideHover(document, position, token) {
+			// Get the word at current position
+			const range = document.getWordRangeAtPosition(position);
+            const word = document.getText(range);
+
+			// Check for label
+			if (/^(\w|\\)+:$/.test(document.lineAt(position).text)) {
+				// Is Label
+				return new vscode.Hover(`\`\`\`evscript\n(Label) ${word}:\n\`\`\``)
+			}
+
+			// Check for parameter
+			if (range.start.character > 0) {
+				let parameter = document.getText(new vscode.Range(range.start.line, range.start.character - 1, range.end.line, range.end.character))
+				if (/^@\w+$/.test(parameter)) {
+					// Is Work
+					return new vscode.Hover(`\`\`\`evscript\n(Work) ${parameter}\n\`\`\``)
+				} else if (/^\#\w+$/.test(parameter)) {
+					// Is Flag
+					return new vscode.Hover(`\`\`\`evscript\n(Flag) ${parameter}\n\`\`\``)
+				} else if (/^\$\w+$/.test(parameter)) {
+					// Is SysFlag
+					return new vscode.Hover(`\`\`\`evscript\n(SysFlag) ${parameter}\n\`\`\``)
+				}
+			}
+			
+
+			// Check for evCmd
+			for (const evCmdCode in ev_scripts) {
+				if (Object.hasOwnProperty.call(ev_scripts, evCmdCode)) {
+					const evCmd = ev_scripts[evCmdCode];
+					if (evCmd.name == word) {
+						// evCmd Found
+						// Create the method layout
+						let mainLine = `\`\`\`evscript\n(EvCmd) ${evCmd.name}(`
+						let descLine = `${evCmd.description}\n\n`
+						evCmd.validArgs.forEach(arg => {
+							// For each possible argument
+							mainLine += arg.name
+							mainLine += arg.optional ? "?: " : ": "
+							arg.validArgTypes.forEach(argType => {
+								// For each arument type an argument can be
+								mainLine += argTypes[argType] + " | "
+							});
+							mainLine = mainLine.substring(0, mainLine.length - 3)
+							mainLine += ", "
+							descLine += `@param \`${arg.name}\` - ${arg.description}  \n\n`
+						});
+						mainLine = evCmd.validArgs.length > 0 ? mainLine.substring(0, mainLine.length - 2) : mainLine
+						mainLine += ")\n\`\`\`"
+						return new vscode.Hover([mainLine, descLine])
+					}
+				}
+				// Process Cancellations
+				if (token.isCancellationRequested) {
+					return
+				}
+			}
+
+			// Return nothing found
+			return
+		}
 	}))
 }
 
